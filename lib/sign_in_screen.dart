05e6a72cf8f9352615368ebafc590cbb9fd839cf
Mr Tech <sikanderbakhat2017@gmail.com>
2023-08-16 01:27:42 +0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/home_screen.dart

--- lib/sign_in_screen.dart
+++ lib/sign_in_screen.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:smart_patient_monitoring_system/Utils/utils.dart';
 import 'package:smart_patient_monitoring_system/home_screen.dart';
 import 'package:smart_patient_monitoring_system/sign_up.dart';
+import 'package:smart_patient_monitoring_system/widgets/rounded_button.dart';
+
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({Key? key}) : super(key: key);
@@ -10,123 +15,157 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  bool loading = false;
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final _auth = FirebaseAuth.instance;
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  void login() {
+    setState(() {
+      loading = true;
+    });
+    _auth
+        .signInWithEmailAndPassword(
+        email: emailController.text, password: passwordController.text)
+        .then((value) {
+      Utils().toastMessage(value.user!.email.toString());
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+      setState(() {
+        loading = false;
+      });
+    }).onError((error, stackTrace) {
+      debugPrint(error.toString());
+      Utils().toastMessage(error.toString());
+      setState(() {
+        loading = false;
+      });
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/images/background.jpg'),
-              fit: BoxFit.cover)),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Stack(
-          children: [
-            Container(
-              padding: const EdgeInsets.only(left: 35, top: 130),
-              child: const Text(
-                "Welcome\nBack",
-                style: TextStyle(color: Colors.white, fontSize: 33),
+    return WillPopScope(
+      onWillPop: ()async{
+        SystemNavigator.pop();
+        return true;
+      },
+      child: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/background.jpg'),
+                fit: BoxFit.cover)),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 60,
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 200,
+                    width: 200,
+                    child: Image.asset(
+                      "assets/images/unilogo.png",
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Text(
+                    "SMART PATIENT\nMONITORING SYSTEM",textAlign:TextAlign.center,
+                    style: TextStyle(fontWeight:FontWeight.bold,fontSize: 34,color: Colors.black26),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            controller: emailController,
+                            decoration: InputDecoration(
+                                hintText: 'Email',
+                                prefixIcon: const Icon(Icons.email_outlined),
+                                prefixIconColor: Colors.blueAccent,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                )),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Enter email';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          TextFormField(
+                            keyboardType: TextInputType.text,
+                            controller: passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                                hintText: 'Password',
+                                prefixIcon: const Icon(Icons.password_sharp),
+                                prefixIconColor: Colors.blueAccent,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                )),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Enter password';
+                              }
+                              return null;
+                            },
+                          ),
+                        ],
+                      )),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  RoundButton(
+                    loading: loading,
+                    title: 'Login',
+                    onTap: () {
+                      if (_formKey.currentState!.validate()) {
+                        login();
+                      }
+                    },
+                  ),
+                const SizedBox(
+                  height: 30,
+                ),
+                  RoundButton(
+                    title: 'SignUp',
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignUp()));
+                    },
+                  ),
+                ],
               ),
             ),
-            SingleChildScrollView(
-              child: Container(
-                padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height * 0.35,
-                    right: 35,
-                    left: 35),
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                          fillColor: Colors.grey.shade100,
-                          filled: true,
-                          hintText: 'Email',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          )),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          fillColor: Colors.grey.shade100,
-                          filled: true,
-                          hintText: 'Password',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          )),
-                    ),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          'Signin',
-                          style: TextStyle(
-                              fontSize: 27, fontWeight: FontWeight.w700),
-                        ),
-                        CircleAvatar(
-                          radius: 30,
-                          backgroundColor: Colors.grey,
-                          child: IconButton(
-                            onPressed: () {
-                              _navigateToHomeScreen(context);
-                            },
-                            icon: const Icon(Icons.arrow_forward),
-                          ),
-                        )
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        TextButton(
-                            onPressed: () {
-                              _navigateToSignUp(context);
-                            },
-                            child: const Text(
-                              'SignUp',
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                fontSize: 20,
-                                color: Colors.blue,
-                              ),
-                            )),
-                        TextButton(
-                            onPressed: () {},
-                            child: const Text(
-                              'Forget Password',
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                fontSize: 20,
-                                color: Colors.blue,
-                              ),
-                            ))
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
   }
-}
-void _navigateToSignUp(BuildContext context) {
-  Navigator.of(context)
-      .push(MaterialPageRoute(builder: (context) => const SignUp()));
-}
-
-void _navigateToHomeScreen(BuildContext context) {
-  Navigator.of(context)
-      .push(MaterialPageRoute(builder: (context) => const HomeScreen()));
 }
--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:smart_patient_monitoring_system/Utils/utils.dart';
 import 'package:smart_patient_monitoring_system/home_screen.dart';
 import 'package:smart_patient_monitoring_system/sign_in_screen.dart';
+import 'package:smart_patient_monitoring_system/widgets/rounded_button.dart';
+
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -10,167 +15,177 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  bool loading = false;
+  final _formKey = GlobalKey<FormState>();
+  final userController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final _auth = FirebaseAuth.instance;
+
+  @override
+  void dispose() {
+    super.dispose();
+    userController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  void signup(){
+    setState(() {
+      loading = true;
+    });
+    _auth
+        .createUserWithEmailAndPassword(
+        email: emailController.text.toString(),
+        password: passwordController.text.toString())
+        .then((value) {
+      setState(() {
+        loading = false;
+      });
+    }).onError((error, stackTrace) {
+      debugPrint(error.toString());
+      Utils().toastMessage(error.toString());
+      setState(() {
+        loading = false;
+      });
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/images/background.jpg'),
-              fit: BoxFit.cover)),
-      child: Scaffold(
-        appBar: AppBar(
+    return WillPopScope(
+      onWillPop: ()async{
+        SystemNavigator.pop();
+        return true;
+      },
+      child: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/background.jpg'),
+                fit: BoxFit.cover)),
+        child: Scaffold(
           backgroundColor: Colors.transparent,
-          elevation: 0,
-        ),
-        backgroundColor: Colors.transparent,
-        body: Stack(
-          children: [
-            Container(
-              padding: const EdgeInsets.only(left: 35, top: 20),
-              child: const Text(
-                "Create \n Account",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 33,
-                    fontWeight: FontWeight.bold),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 60,
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 200,
+                    width: 200,
+                    child: Image.asset(
+                      "assets/images/unilogo.png",
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Text(
+                    "SMART PATIENT\nMONITORING SYSTEM",textAlign:TextAlign.center,
+                    style: TextStyle(fontWeight:FontWeight.bold,fontSize: 34,color: Colors.black26),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            controller: userController,
+                            decoration: InputDecoration(
+                                hintText: 'User Name',
+                                prefixIcon: const Icon(Icons.person),
+                                prefixIconColor: Colors.blueAccent,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                )),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Enter Your Name';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            controller: emailController,
+                            decoration: InputDecoration(
+                                hintText: 'Email',
+                                prefixIcon: const Icon(Icons.email_outlined),
+                                prefixIconColor: Colors.blueAccent,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                )),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Enter email';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          TextFormField(
+                            keyboardType: TextInputType.text,
+                            controller: passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                                hintText: 'Password',
+                                prefixIcon: const Icon(Icons.password_sharp),
+                                prefixIconColor: Colors.blueAccent,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                )),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Enter password';
+                              }
+                              return null;
+                            },
+                          ),
+                        ],
+                      )),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  RoundButton(
+                    loading: loading,
+                    title: 'Sign Up',
+                    onTap: () {
+                      if (_formKey.currentState!.validate()) {
+                        signup();
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  RoundButton(
+                    title: 'Already have an Account',
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignInScreen()));
+                    },
+                  ),
+                ],
               ),
             ),
-            SingleChildScrollView(
-              child: Container(
-                padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height * 0.2,
-                    right: 35,
-                    left: 35),
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: const BorderSide(
-                                color: Colors.black,
-                              )),
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: const BorderSide(
-                                color: Colors.white,
-                              )),
-                          hintText: 'Name',
-                          hintStyle: const TextStyle(
-                            color: Colors.white,
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          )),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: const BorderSide(
-                                color: Colors.black,
-                              )),
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: const BorderSide(
-                                color: Colors.white,
-                              )),
-                          hintText: 'Email',
-                          hintStyle: const TextStyle(
-                            color: Colors.white,
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          )),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: const BorderSide(
-                                color: Colors.black,
-                              )),
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: const BorderSide(
-                                color: Colors.white,
-                              )),
-                          hintText: 'Password',
-                          hintStyle: const TextStyle(
-                            color: Colors.white,
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          )),
-                    ),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          'SignUp',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 27,
-                              fontWeight: FontWeight.w700),
-                        ),
-                        CircleAvatar(
-                          radius: 30,
-                          backgroundColor: Colors.grey,
-                          child: IconButton(
-                            onPressed: () {
-                              _navigateToHomeScreen(context);
-                            },
-                            icon: const Icon(Icons.arrow_forward),
-                          ),
-                        )
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        TextButton(
-                            onPressed: () {
-                              _navigateToSignIn(context);
-                            },
-                            child: const Text(
-                              'Already have an account',
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                fontSize: 20,
-                                color: Colors.blue,
-                              ),
-                            )),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
   }
-}
-
-void _navigateToSignIn(BuildContext context) {
-  Navigator.of(context)
-      .push(MaterialPageRoute(builder: (context) => const SignInScreen()));
-}
-void _navigateToHomeScreen(BuildContext context) {
-  Navigator.of(context)
-      .push(MaterialPageRoute(builder: (context) => const HomeScreen()));
 }
